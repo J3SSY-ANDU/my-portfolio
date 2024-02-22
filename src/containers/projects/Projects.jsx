6b164f5e94c7e62ee47aa715651473f7a3792f46
Jessy Andujar Cruz <124212160+J3SSY-ANDU@users.noreply.github.com>
2024-02-22 12:55:48 -0400

    side-bar eliminated

--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -1,69 +1,62 @@
-import React, { useState } from 'react';
-import CodeSection from '../../components/code-section/CodeSection'
+import React from 'react';
 import portfolioImage from '../../assets/portfolio.png';
+import Project from '../../components/project/Project';
 import './projects.css';
 
 const projectsList = {
-    react: {
-        name: "React",
-        projects: [ {
-                title: "Portfolio",
-                backgroundImage: portfolioImage,
-                description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
-                link: ''
-            },   
-            {
-                title: "GPT-3",
-                backgroundImage: portfolioImage,
-                description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
-                link: ''
-            },
-            {
-                title: "GERISH",
-                backgroundImage: portfolioImage,
-                description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
-                link: ''
-            },
-            {
-                title: "Youtube",
-                backgroundImage: portfolioImage,
-                description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
-                link: ""
-            },
-            {
-                title: "AirBnb",
-                backgroundImage: portfolioImage,
-                description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
-                link: ""
-            },
-            {
-                title: "Canvas",
-                backgroundImage: portfolioImage,
-                description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
-                link: ""
-            }
-        ]
-        
-    }
+    projects: [ {
+            title: "Portfolio",
+            backgroundImage: portfolioImage,
+            description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
+            link: ''
+        },   
+        {
+            title: "GPT-3",
+            backgroundImage: portfolioImage,
+            description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
+            link: ''
+        },
+        {
+            title: "GERISH",
+            backgroundImage: portfolioImage,
+            description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
+            link: ''
+        },
+        {
+            title: "Youtube",
+            backgroundImage: portfolioImage,
+            description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
+            link: ""
+        },
+        {
+            title: "AirBnb",
+            backgroundImage: portfolioImage,
+            description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
+            link: ""
+        },
+        {
+            title: "Canvas",
+            backgroundImage: portfolioImage,
+            description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
+            link: ""
+        }
+    ]
 }
 
 function Projects() {
-    const [toggleReact, setToggleReact] = useState(true);
-
     return (
         <div className='portfolio__projects section__padding' id='projects'>
             <div className='portfolio__projects-container section__width' >
-                <div className='portfolio__projects-container_heading'></div>
+                <div className='portfolio__projects-container_heading'>
                     <h1>Projects</h1>
-                    <div className='portfolio__projects-container_heading-sideBar'>
-                        <div onClick={ () => setToggleReact(!toggleReact) } style={{ background: toggleReact ? "#ffffff1a" : "" }}>
-                            <p>{projectsList.react.name}</p>
-                        </div>
-                    </div>
+                    <p>Here is the more recent work that I've done. <a href='#hire-me'>If you need a personalize project, message me!</a></p>
+                </div>
                 <div className='portfolio__projects-container_body'>
-                    {toggleReact &&
-                        <CodeSection projectsList={projectsList} />
-                    }
+                    {projectsList.projects.map((project, i) => {
+                        return (
+                            <Project title={project.title} backgroundImage={project.backgroundImage} description={project.description} link={project.link} />
+                        )
+                    })}
                 </div>
             </div>
         </div>
