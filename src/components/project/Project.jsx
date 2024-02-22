520cca22d3a84246fe2e7d5751ee221bdd851925
J3SSY-ANDU <jessy.andujar@upr.edu>
2024-02-22 13:45:57 -0400

    styling projects

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './project.css';
 
-function Project({ title, backgroundImage, description, link }) {
+function Project({ title, backgroundImage, description, codeIn, link }) {
     return (
         <div className='portfolio__project'>
             <div className='portfolio__project-container'>
                 <h3>{title}</h3>
                 <p>{description}</p>
+                <p>{codeIn}</p>
             </div>
         </div>
     )
--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -8,36 +8,42 @@ const projectsList = {
             title: "Portfolio",
             backgroundImage: portfolioImage,
             description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
+            codeIn: "React / JSX",
             link: ''
         },   
         {
             title: "GPT-3",
             backgroundImage: portfolioImage,
             description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
+            codeIn: "React",
             link: ''
         },
         {
             title: "GERISH",
             backgroundImage: portfolioImage,
             description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
+            codeIn: "React",
             link: ''
         },
         {
             title: "Youtube",
             backgroundImage: portfolioImage,
             description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
+            codeIn: "React",
             link: ""
         },
         {
             title: "AirBnb",
             backgroundImage: portfolioImage,
             description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
+            codeIn: "React",
             link: ""
         },
         {
             title: "Canvas",
             backgroundImage: portfolioImage,
             description: "Lorem ipsum dolor sit amet consectetur. Pellentesque nisl pellentesque in id tortor ultrices eu scelerisque elit.",
+            codeIn: "React",
             link: ""
         }
     ]
@@ -49,12 +55,12 @@ function Projects() {
             <div className='portfolio__projects-container section__width' >
                 <div className='portfolio__projects-container_heading'>
                     <h1>Projects</h1>
-                    <p>Here is the more recent work that I've done. <a href='#hire-me'>If you need a personalize project, message me!</a></p>
+                    <p>Here is some of my latest work. If you require a personalized project, <a href='#hire-me'>feel free to reach out to me!</a></p>
                 </div>
                 <div className='portfolio__projects-container_body'>
                     {projectsList.projects.map((project, i) => {
                         return (
-                            <Project title={project.title} backgroundImage={project.backgroundImage} description={project.description} link={project.link} />
+                            <Project title={project.title} backgroundImage={project.backgroundImage} description={project.description} link={project.link} codeIn={project.codeIn} key={project.title + i}/>
                         )
                     })}
                 </div>
